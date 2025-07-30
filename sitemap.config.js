d51fac30303e300a9643d8c5ecdecf32ab8ae67d
rhythm-cyber <rhythm.deals2026@gmail.com>
2025-07-30 17:26:47 +0530

    Create sitemap.config.js

---
+++ sitemap.config.js
@@ -0,0 +1,9 @@
+// sitemap.config.js
+module.exports = {
+  siteUrl: "https://deals89.store", // ← yeh tumhara actual domain hai
+  generateRobotsTxt: true,
+  changefreq: "daily",
+  priority: 0.7,
+  sitemapSize: 5000,
+  outDir: "./public",
+};
